704e03d8c6675fcc30466b71abd7d754a20bd0e2
stephbaltimore <stephbaltimore@gmail.com>
2021-02-11 19:07:33 -0500

    working with parcel etc

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -479,7 +479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50840" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49982" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
