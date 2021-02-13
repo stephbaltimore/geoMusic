20748cdfaa850a6b981065ddd45be808ab654c38
stephbaltimore <stephbaltimore@gmail.com>
2021-02-13 11:05:40 -0500

    added placeholder for top city states

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -29559,7 +29559,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55261" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49616" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
