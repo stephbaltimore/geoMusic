5fb0b842b22c78900fc9a96f49a1c4949617d071
stephbaltimore <stephbaltimore@gmail.com>
2021-02-14 11:05:48 -0500

    uninstalled parcel and reinstalled

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -29588,7 +29588,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64786" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51586" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
